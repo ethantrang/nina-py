137fc7bb268109d514ec6de873e2d985383eed9c
ethantrang <ethan.trang5521@gmail.com>
2024-07-11 12:52:00 +0700

    update keys

--- main.py
+++ main.py
@@ -118,5 +118,7 @@ async def chat_endpoint(chat_history: ChatHistory):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=3001, reload=True)
+
